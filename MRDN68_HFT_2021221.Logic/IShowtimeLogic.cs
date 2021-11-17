afcdbcd659f5334d7bd8b98a7b22f15772aab13a
OroszDan <orosz.dani18@gmail.com>
2021-11-17 19:50:45 +0100

    ShowtimeLogic class and interface created

---
+++ MRDN68_HFT_2021221.Logic/IShowtimeLogic.cs
@@ -0,0 +1,17 @@
+﻿using MRDN68_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRDN68_HFT_2021221.Logic
+{
+    interface IShowtimeLogic
+    {
+        void Create(Showtime showtime);
+        IQueryable<Showtime> ReadAll();
+        void Update(Showtime showtime);
+        void Delete(int showtimeId);
+    }
+}
---
+++ MRDN68_HFT_2021221.Logic/ShowtimeLogic.cs
@@ -0,0 +1,43 @@
+﻿using MRDN68_HFT_2021221.Models;
+using MRDN68_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRDN68_HFT_2021221.Logic
+{
+    class ShowtimeLogic : IShowtimeLogic
+    {
+        IShowtimeRepository repo;
+
+        public ShowtimeLogic(IShowtimeRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        //
+       
+
+        public void Create(Showtime showtime)
+        {
+            repo.Create(showtime);
+        }
+
+        public void Delete(int showtimeId)
+        {
+            repo.Delete(showtimeId);
+        }
+
+        public IQueryable<Showtime> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(Showtime showtime)
+        {
+            repo.Update(showtime);
+        }
+    }
+}
