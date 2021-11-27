22cf336a507c2c6685c2a097787bac0854f2e261
OroszDan <orosz.dani18@gmail.com>
2021-11-27 18:45:23 +0100

    transients added

--- MRDN68_HFT_2021221.Endpoint/Startup.cs
+++ MRDN68_HFT_2021221.Endpoint/Startup.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MRDN68_HFT_2021221.Data;
+using MRDN68_HFT_2021221.Logic;
+using MRDN68_HFT_2021221.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +16,26 @@ namespace MRDN68_HFT_2021221.Endpoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddTransient<IShowtimeLogic, ShowtimeLogic>();
+            services.AddTransient<IMovieLogic, MovieLogic>();
+            services.AddTransient<IDirectorLogic, DirectorLogic>();
+
+            services.AddTransient<IShowtimeRepository, ShowtimeRepository>();
+            services.AddTransient<IMovieRepository, MovieRepository>();
+            services.AddTransient<IDirectorRepository, DirectorRepository>();
+
+            services.AddTransient<MovieProgrammeDbContext, MovieProgrammeDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +44,10 @@ namespace MRDN68_HFT_2021221.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
             }
 
             app.UseRouting();
