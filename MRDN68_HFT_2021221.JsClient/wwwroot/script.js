49a51c12f9830231e351a70fa912472fafc76660
OroszDan <orosz.dani18@gmail.com>
2022-04-29 17:38:26 +0200

    create display done

--- MRDN68_HFT_2021221.JsClient/wwwroot/script.js
+++ MRDN68_HFT_2021221.JsClient/wwwroot/script.js
@@ -1,16 +1,46 @@
-﻿let movies = [];
+﻿let director = [];
 
-fetch('http://localhost:65512/movie')
-    .then(x => x.json())
-    .then(y => {
-        movies = y;
-        console.log(movies);
-        display();
-    });
+getdata();
+
+async function getdata() {
+  await fetch('http://localhost:65512/director')
+        .then(x => x.json())
+        .then(y => {
+            director = y;
+            console.log(director);
+            display();
+        });
+}
+
 
 function display() {
-    movies.forEach(t => {
+    director.forEach(t => {
         console.log(t.name);
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.id + "</td><td>"
+            + t.name + "</td><td>"
+        + t.birthYear + "</td></tr>";
     });
 
 }
+
+function create()
+{
+    let name = document.getElementById('directorname').value;
+    let year = document.getElementById('directoryear').value;
+    fetch('http://localhost:65512/director', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { name: name, birthYear: year })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+   
+
+}
+
